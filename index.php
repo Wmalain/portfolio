bcdcfb6bfa25a54cbfa68dba971f66a73de67458
Alain Wehbe <wehbewmalain@gmail.com>
2021-03-16 12:55:21 +0100

    second commit

--- index.php
+++ index.php
@@ -9,6 +9,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=DotGothic16&family=Indie+Flower&family=Lato&family=Redressed&family=Roboto+Mono&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+
+
 
 </head>
 
@@ -17,36 +20,19 @@
 <h1 class="headerh1">Alain <strong>WEHBE</strong></h1>
 <h2 class="headerh2"></h2>
 </div>
-<!-- <div class="div2header">
-<ul class="nav">
-<li class="liheader">Presentation</li>
-<li class="liheader">Projets</li>
-<li class="liheader">connaissances</li>
-<li class="liheader">Contact</li>
-</ul>
-</div>  -->
 
-<!-- Presentation -->
-<!-- <h2 class="presh2">Présentation</h2>
-<div class="presdiv">
-    <p class="presep">Moi c’est Alain Wehbe, passionné de nouvelle technologie depuis mon plus jeune âge, Adolescent je découvre INTERNET, les sites de l’époque étaient bien différents de ce qu’ils sont aujourd’hui. Je vous propose de mettre cette passion au service de VOS besoins, pour faire le site de VOS rêves !</p>
-<img class="presimg" src="assets/img/pro.jpg" alt="">
-</div> -->
-
-
-<!-- Projet -->
 
 <ul class="nav nav-tabs div2header" id="myTab" role="tablist">
   <li class="nav-item" role="presentation">
-    <button class="nav-link active" id="Presentation-tab" data-bs-toggle="tab" data-bs-target="#Presentation" type="button" role="tab" aria-controls="Presentation" aria-selected="true">Presentation</button>
+    <button class="nav-link active butnav" id="Presentation-tab" data-bs-toggle="tab" data-bs-target="#Presentation" type="button" role="tab" aria-controls="Presentation" aria-selected="true">Presentation</button>
   </li>
-  <li class="nav-item" role="presentation">
+  <li class="nav-item butnav" role="presentation">
     <button class="nav-link" id="projets-tab" data-bs-toggle="tab" data-bs-target="#projets" type="button" role="tab" aria-controls="projets" aria-selected="false">Projets</button>
   </li>
-  <li class="nav-item" role="presentation">
+  <li class="nav-item butnav" role="presentation">
     <button class="nav-link" id="connaissance-tab" data-bs-toggle="tab" data-bs-target="#connaissance" type="button" role="tab" aria-controls="connaissance" aria-selected="false">Connaissances</button>
   </li>
-  <li class="nav-item" role="presentation">
+  <li class="nav-item butnav" role="presentation">
     <button class="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact" type="button" role="tab" aria-controls="contact" aria-selected="false">Contact</button>
   </li>
 </ul>
@@ -200,7 +186,19 @@
 
 <!-- contact -->
 
-<footer></footer>
+<div>
+
+  <div class="foot1"> Voir un monde dans un grain de sable Et un Ciel dans une Fleur sauvage Tenir l'Infini dans la paume de la main Et l'éternité dans une heure.</div>
+  
+  <div class="foot2">
+    <p class="footp">© Alain Wehbe 2021</p>
+    <a href="https://www.linkedin.com/in/alain-wehbe/" target="_blank"><img src="" alt=""><i class="fab fa-linkedin logo"></i></a>
+    <a href="https://github.com/Wmalain" target="_blank"><i class="fab fa-github logo"></i></a>
+  </div>
+</div>
+
+
+</div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 <body>
     
